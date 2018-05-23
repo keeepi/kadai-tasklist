0029b40cf23595181ca26f2c6d3831acc96f69e8
EC2 Default User <ec2-user@ip-172-31-38-63.us-east-2.compute.internal>
2018-05-23 04:09:12 +0000

    Implement Kadai Tasklist

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>タスク一覧</h1>
+    
+    @if (count($tasks) > 0)
+        <ul>
+             @foreach ($tasks as $task)
+                <li>{{ $task->content }}</li>
+            @endforeach
+            
+            @foreach ($tasks as $task)
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
+                {!! link_to_route('tasks.create', '新規タスクの投稿') !!}
+            
+            @endforeach
+        </ul>
+    @endif
+   
+
+@endsection
